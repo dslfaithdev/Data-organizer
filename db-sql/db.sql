9ad5036c2aa9509d2203a736fce757ab08e4b7b8
Fredrik Erlandsson <fredrik.e@gmail.com>
2013-07-18 20:40:29 +0000

    Updated database structure

--- db-sql/db.sql
+++ db-sql/db.sql
@@ -1,30 +1,163 @@
-DROP TABLE IF EXISTS message_tags CASCADE;
-DROP TABLE IF EXISTS story_tags CASCADE;
-DROP TABLE IF EXISTS with_tag CASCADE;
-DROP TABLE IF EXISTS post CASCADE;
-DROP TABLE IF EXISTS `comment` CASCADE;
-DROP TABLE IF EXISTS likedby CASCADE;
-DROP TABLE IF EXISTS application CASCADE;
-DROP TABLE IF EXISTS fb_user CASCADE;
-DROP TABLE IF EXISTS place CASCADE;
-DROP TABLE IF EXISTS page CASCADE;
+-- MySQL dump 10.13  Distrib 5.5.30, for FreeBSD9.0 (amd64)
+--
+-- Host: 194.47.148.113    Database: sincere
+-- ------------------------------------------------------
+-- Server version	5.5.30-MariaDB-log
 
-CREATE TABLE `page` (
-  `id` bigint(20) NOT NULL,
-  `name` text,
-  `category` text,
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `application`
+--
+
+DROP TABLE IF EXISTS `application`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `application` (
+  `id` bigint(20) NOT NULL DEFAULT '0',
+  `name` varchar(256) DEFAULT NULL,
+  `namespace` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `comment`
+--
 
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `id` bigint(20) NOT NULL DEFAULT '0',
+  `post_id` bigint(20) NOT NULL DEFAULT '0',
+  `page_id` bigint(20) NOT NULL DEFAULT '0',
+  `fb_id` bigint(20) DEFAULT NULL,
+  `message` text,
+  `can_remove` tinyint(1) DEFAULT NULL,
+  `created_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`page_id`,`post_id`,`id`),
+  KEY `comment_id` (`id`),
+  KEY `fb_id` (`fb_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `crawl_stat`
+--
+
+DROP TABLE IF EXISTS `crawl_stat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `crawl_stat` (
+  `page_id` bigint(20) NOT NULL DEFAULT '0',
+  `name` text CHARACTER SET utf8,
+  `max(post.created_time)` timestamp NULL DEFAULT NULL,
+  `entropy` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `fb_user`
+--
+
+DROP TABLE IF EXISTS `fb_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `fb_user` (
   `id` bigint(20) NOT NULL,
   `name` text,
   `category` bit(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `likedby`
+--
 
+DROP TABLE IF EXISTS `likedby`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `likedby` (
+  `page_id` bigint(20) NOT NULL,
+  `post_id` bigint(20) NOT NULL,
+  `comment_id` bigint(20) NOT NULL DEFAULT '0',
+  `fb_id` bigint(20) NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`page_id`,`post_id`,`comment_id`,`fb_id`),
+  KEY `fb_id` (`fb_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `message_tags`
+--
+
+DROP TABLE IF EXISTS `message_tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `message_tags` (
+  `id` bigint(20) NOT NULL DEFAULT '0',
+  `page_id` bigint(20) DEFAULT NULL,
+  `post_id` bigint(20) DEFAULT NULL,
+  `comment_id` bigint(20) DEFAULT NULL,
+  `offset` int(11) DEFAULT NULL,
+  `length` int(11) DEFAULT NULL,
+  `type` varchar(256) DEFAULT NULL,
+  `name` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `page`
+--
+
+DROP TABLE IF EXISTS `page`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `page` (
+  `id` bigint(20) NOT NULL,
+  `name` text,
+  `category` text,
+  PRIMARY KEY (`id`),
+  KEY `page_id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `place`
+--
+
+DROP TABLE IF EXISTS `place`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `place` (
+  `id` bigint(20) NOT NULL DEFAULT '0',
+  `name` varchar(256) DEFAULT NULL,
+  `loc_latitude` float DEFAULT NULL,
+  `loc_longitude` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `post` (
   `id` bigint(20) NOT NULL DEFAULT '0',
   `page_id` bigint(20) NOT NULL DEFAULT '0',
@@ -46,7 +179,7 @@ CREATE TABLE `post` (
   `comments_count` int(11) DEFAULT NULL,
   `entr_pg` float DEFAULT NULL,
   `entr_ug` float DEFAULT NULL,
-  `object_id` bigint(20) DEFAULT NULL,
+  `object_id` varchar(40) DEFAULT NULL,
   `status_type` varchar(256) DEFAULT NULL,
   `source` varchar(256) DEFAULT NULL,
   `is_hidden` tinyint(1) DEFAULT NULL,
@@ -54,82 +187,19 @@ CREATE TABLE `post` (
   `place_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`page_id`,`id`),
   KEY `post_id` (`id`),
-  CONSTRAINT `post_ibfk_1` FOREIGN KEY (`page_id`) REFERENCES `page` (`id`) ON UPDATE CASCADE
+  KEY `created_time_index` (`created_time`),
+  KEY `comments_count_index` (`comments_count`),
+  KEY `from_id_index` (`from_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `shares`
+--
 
-CREATE TABLE `application` (
-  `id` bigint(20) NOT NULL DEFAULT '0',
-  `name` varchar(256) DEFAULT NULL,
-  `namespace` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `story_tags` (
-  `id` bigint(20) DEFAULT NULL,
-  `page_id` bigint(20) DEFAULT NULL,
-  `post_id` bigint(20) DEFAULT NULL,
-  `comment_id` bigint(20) DEFAULT NULL,
-  `offset` int(11) DEFAULT NULL,
-  `length` int(11) DEFAULT NULL,
-  `type` varchar(256) DEFAULT NULL,
-  `name` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `message_tags` (
-  `id` bigint(20) DEFAULT NULL,
-  `page_id` bigint(20) DEFAULT NULL,
-  `post_id` bigint(20) DEFAULT NULL,
-  `comment_id` bigint(20) DEFAULT NULL,
-  `offset` int(11) DEFAULT NULL,
-  `length` int(11) DEFAULT NULL,
-  `type` varchar(256) DEFAULT NULL,
-  `name` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `place` (
-  `id` bigint(20) DEFAULT NULL,
-  `name` varchar(256) DEFAULT NULL,
-  `loc_latitude` float DEFAULT NULL,
-  `loc_longitude` float DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `with_tag` (
-  `page_id` bigint(20) DEFAULT NULL,
-  `post_id` bigint(20) DEFAULT NULL,
-  `fb_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`page_id`, `post_id`, `fb_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `comment` (
-  `id` bigint(20) NOT NULL DEFAULT '0',
-  `post_id` bigint(20) NOT NULL DEFAULT '0',
-  `page_id` bigint(20) NOT NULL DEFAULT '0',
-  `fb_id` bigint(20) DEFAULT NULL,
-  `message` text,
-  `can_remove` tinyint(1) DEFAULT NULL,
-  `created_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`page_id`,`post_id`,`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `likedby` (
-  `page_id` bigint(20) NOT NULL,
-  `post_id` bigint(20) NOT NULL,
-  `comment_id` bigint(20) NOT NULL DEFAULT '0',
-  `fb_id` bigint(20) NOT NULL,
-  `created_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`page_id`,`post_id`,`comment_id`,`fb_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+DROP TABLE IF EXISTS `shares`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `shares` (
   `id` bigint(20) unsigned NOT NULL,
   `post_id` bigint(20) unsigned NOT NULL,
@@ -138,34 +208,84 @@ CREATE TABLE `shares` (
   `updated_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`,`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/* Useful */
-/* \dt+ -> gives size of tables */
-/* \d+ <table> -> gives table info */
+--
+-- Temporary table structure for view `status`
+--
 
-/* Update the comment count on post */
-UPDATE post SET comments_count = (SELECT count(*) FROM comment WHERE post_id=post.id);
-/* Update the likes count on post */
-UPDATE post SET likes_count = (SELECT count(*) FROM likedby WHERE post_id=post.id);
+DROP TABLE IF EXISTS `status`;
+/*!50001 DROP VIEW IF EXISTS `status`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `status` (
+  `Table Name` tinyint NOT NULL,
+  `# Rows` tinyint NOT NULL,
+  `Total Size MB` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
 
-/* Insert ignore */
-CREATE OR REPLACE RULE "insert_ignore" AS ON INSERT TO 
-	fb_user WHERE EXISTS(SELECT true FROM fb_user WHERE id = NEW.id) DO INSTEAD NOTHING;
+--
+-- Table structure for table `story_tags`
+--
 
-CREATE OR REPLACE RULE "insert_ignore" AS ON INSERT TO 
-	likedby WHERE (new.page_id, new.post_id, new.comment_id, new.fb_id) IN 
-	( SELECT page_id, post_id, comment_id, fb_id FROM likedby WHERE 
-	page_id=new.page_id AND post_id=new.post_id AND comment_id=new.comment_id AND fb_id=new.fb_id) 
-	DO INSTEAD NOTHING;
+DROP TABLE IF EXISTS `story_tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `story_tags` (
+  `id` bigint(20) NOT NULL DEFAULT '0',
+  `page_id` bigint(20) DEFAULT NULL,
+  `post_id` bigint(20) DEFAULT NULL,
+  `comment_id` bigint(20) DEFAULT NULL,
+  `offset` int(11) DEFAULT NULL,
+  `length` int(11) DEFAULT NULL,
+  `type` varchar(256) DEFAULT NULL,
+  `name` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/* insert on duplicate update */
-CREATE OR REPLACE RULE "insert_on_duplicate_update" AS ON INSERT TO 
-	likedby WHERE (new.page_id, new.post_id, new.comment_id, new.fb_id) IN 
-	( SELECT page_id, post_id, comment_id, fb_id FROM likedby WHERE 
-	page_id=new.page_id AND post_id=new.post_id AND comment_id=new.comment_id AND fb_id=new.fb_id) 
-	DO INSTEAD UPDATE likedby SET created_time=new.created_time
-	WHERE page_id=new.page_id AND post_id=new.post_id AND comment_id=new.comment_id AND fb_id=new.fb_id;
+--
+-- Table structure for table `with_tags`
+--
 
-CREATE OR REPLACE RULE "insert_on_duplicate_update" AS ON INSERT TO 
-	fb_user WHERE EXISTS(SELECT true FROM fb_user WHERE id = NEW.id) DO INSTEAD 
-	UPDATE fb_uder SET name = new.name, category = new.category WHERE fb_user = new.fb_user;
+DROP TABLE IF EXISTS `with_tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `with_tags` (
+  `page_id` bigint(20) NOT NULL DEFAULT '0',
+  `post_id` bigint(20) NOT NULL DEFAULT '0',
+  `fb_id` bigint(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`page_id`,`post_id`,`fb_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Final view structure for view `status`
+--
+
+/*!50001 DROP TABLE IF EXISTS `status`*/;
+/*!50001 DROP VIEW IF EXISTS `status`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `status` AS (select `information_schema`.`TABLES`.`TABLE_NAME` AS `Table Name`,format(`information_schema`.`TABLES`.`TABLE_ROWS`,0) AS `# Rows`,format(round(((`information_schema`.`TABLES`.`DATA_LENGTH` + `information_schema`.`TABLES`.`INDEX_LENGTH`) / (1024 * 1024)),2),2) AS `Total Size MB` from `information_schema`.`TABLES` where (`information_schema`.`TABLES`.`TABLE_SCHEMA` = 'sincere')) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-07-18 20:35:16
